b43b09df4ce116e5c568fa8dd0ff486f9f96b18e
Daniel Ramos <daniel.r.ramos@icloud.com>
2020-05-23 23:50:21 +0100

    Update checker.py

--- distinguisher/checker.py
+++ distinguisher/checker.py
@@ -7,6 +7,16 @@ class ModelChecker:
 
     def generate_symbolic_representation(self, programs):
         pass
+    
+
+class Template:
+
+    def __init__(self, path_to_impl):
+        with open(path_to_impl, "r") as f:
+            self.template = f.read()
+
+    def genarate_code(self, programs, input_constraints):
+        pass
 
 
 class CBMC(ModelChecker):
