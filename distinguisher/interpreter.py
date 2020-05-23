309f22bf735cf025c03aa2bed8fd380f524f2cf8
Daniel Ramos <daniel.r.ramos@icloud.com>
2020-05-23 23:55:04 +0100

    Delete interpreter.py

--- distinguisher/interpreter.py
+++
@@ -1,7 +0,0 @@
-class ModelInterpreter:
-
-    def extract_input(self, symbolic_representation, model):
-        pass
-
-    def extract_output(self, symbolic_representation, model, idx):
-        pass
