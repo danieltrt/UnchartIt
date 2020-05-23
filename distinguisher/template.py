b237bba1c45ae7265e494f28b5d311c2f63307a3
Daniel Ramos <daniel.r.ramos@icloud.com>
2020-05-23 23:51:42 +0100

    Delete template.py

--- distinguisher/template.py
+++
@@ -1,8 +0,0 @@
-class Template:
-
-    def __init__(self, path_to_impl):
-        with open(path_to_impl, "r") as f:
-            self.template = f.read()
-
-    def genarate_code(self, programs, input_constraints):
-        pass
