eae21f2fe5097bef56a0e0c3a7d89b177f20449c
Daniel Ramos <blue@danube.localdomain>
2020-10-16 15:16:39 +0100

    Add synthesis functionality

--- web/synth/urls.py
+++ web/synth/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'synth'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload'),
+    path('programs/<int:id>/', views.programs, name='programs')
 ]
 
--- web/synth/views.py
+++ web/synth/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+import ast
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'synthesizer.html')
+
+
+def upload(request):
+    return HttpResponseRedirect(reverse('synth:programs', args=(1,)))
+
+
+def programs(request, id):
+    return render(request, 'programs.html', {
+        "programs": ['COL0', 'COL1']
+    })
